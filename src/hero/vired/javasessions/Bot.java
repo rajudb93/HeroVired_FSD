e57c5d4e951b3878cadf0ed81c44f83c91995751
Raju Dubey <rajudb93@gmail.com>
2023-05-14 22:26:40 +0530

    created a HelloWorld Java program

---
+++ src/hero/vired/javasessions/Bot.java
@@ -0,0 +1,31 @@
+package hero.vired.javasessions;
+
+import java.util.Scanner;
+
+public class Bot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your name=");
+        String name=sc.nextLine();
+
+        System.out.println("Enter your age=");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("You are eligible to vote");
+        }
+        else{
+            System.out.println("You acan't vote");
+        }
+
+        // System.out.println("Enter your hobby=");
+        // String hobby=sc.nextLine();
+
+        // System.out.println("Enter your mobile number=");
+        // long  contactNumber=sc.nextLong();
+
+
+        
+    }
+    
+}
---
+++ src/hero/vired/javasessions/Sample.java
@@ -0,0 +1,22 @@
+package hero.vired.javasessions;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hi, I am using the Visual Studio Code to write code");
+        System.out.println("This is second line of code in this progra");
+
+        int a=100;
+        int b=200;
+        int c=a*b;
+        int d=a+b;
+        int e=b-a;
+        int f=b/a;
+
+        System.out.println("The produc of "+a+" and "+b+" is="+c);
+        System.out.println("The sum of "+a+" and "+b+" is="+d);
+        System.out.println("The substraction  of "+a+" and "+b+" is="+e);
+        System.out.println("The division of "+a+" and "+b+" is="+f);
+
+    }
+    
+}
