e718d11023901e6da9bd15aa4a0f03c3dac6aad7
Raju Dubey <rajudb93@gmail.com>
2023-05-14 13:21:21 +0530

    Question 2 - Completed the 2nd assignment question related Assignment operator in Java

---
+++ src/hero/vired/javasessions/AssignmentOperator.java
@@ -0,0 +1,23 @@
+package hero.vired.javasessions;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int num1 = 10;
+
+		num1 = num1 + 2;
+		System.out.println("After addition: num = " + num1);
+
+		num1 = num1 - 4;
+		System.out.println("After subtraction: num = " + num1);
+
+		num1 = num1 * 7;
+		System.out.println("After multiplication: num = " + num1);
+
+		num1 = num1 / 4;
+		System.out.println("After division: num = " + num1);
+
+		num1 = num1 % 5;
+		System.out.println("After modulus: num = " + num1);
+	}
+
+}
