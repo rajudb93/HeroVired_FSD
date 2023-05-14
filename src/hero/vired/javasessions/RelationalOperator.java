33fa0f18b515e2ab24f6e53070bb57d0403470c5
Raju Dubey <rajudb93@gmail.com>
2023-05-14 13:15:56 +0530

    Question 1 - Completed the 1st assignment question related Relational operator in Java

--- src/hero/vired/javasessions/RelationalOperator.java
+++ src/hero/vired/javasessions/RelationalOperator.java
@@ -1,7 +1,20 @@
 package hero.vired.javasessions;
+
 public class RelationalOperator {
 	public static void main(String[] args) {
-		
+		int num1 = 10;
+		int num2 = 20;
+
+		boolean equal = num1 == num2;
+		boolean notEqual = num1 != num2;
+		boolean greaterThan = num1 > num2;
+		boolean lessThan = num1 < num2;
+
+		System.out.println("Equal: " + equal);
+		System.out.println("Not Equal: " + notEqual);
+		System.out.println("num1 is Greater Than num2: " + greaterThan);
+		System.out.println("num1 is Less Than num2: " + lessThan);
+
 	}
 
 }
