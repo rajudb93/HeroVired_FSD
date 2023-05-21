7a8d109b54b6dc9f4bd9991e090a60bb3eac9977
Raju Dubey <rajudb93@gmail.com>
2023-05-21 14:31:11 +0530

    implemented logic to calculate factorial of a given number

---
+++ src/hero/vired/javasessions/Factorial.java
@@ -0,0 +1,17 @@
+package hero.vired.javasessions;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n=5;
+        int fact=1;
+
+        while(n>=1){
+            fact=fact*n;
+            n--;
+
+
+        }
+       System.out.println("Factorial="+fact);
+    }
+    
+}
