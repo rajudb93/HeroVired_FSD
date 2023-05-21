cf78c2c43fcf8c01a048b6e902972dc12203ad6d
Raju Dubey <rajudb93@gmail.com>
2023-05-21 14:32:13 +0530

    implemented example program for While loop

---
+++ src/hero/vired/javasessions/WhileLoop.java
@@ -0,0 +1,12 @@
+package hero.vired.javasessions;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
